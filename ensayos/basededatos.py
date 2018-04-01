0c4bf134dc32d4541c945de83337b056b2822430
Santiago <sjaram25@eafit.edu.co>
2018-03-31 21:26:14 -0500

    trabajo de hoy

--- ensayos/basededatos.py
+++ ensayos/basededatos.py
@@ -3,6 +3,6 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 print ("Opened database successfully");
 
-conn.execute('CREATE TABLE students (name TEXT, addr TEXT, city TEXT, pin TEXT)')
+conn.execute('CREATE TABLE reto2 (fecha TEXT, valor INT, clasificacion TEXT')
 print ("Table created successfully");
 conn.close()
--- final.py
+++ final.py
@@ -1,18 +1,30 @@
-from flask import Flask,request, render_template
+from flask import Flask,request,render_template
 
 app = Flask(__name__)
 
-
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET','POST'])
 def add_entry():
-    print("p0")
-    request_json = request.get_json(force=True)
-    value1 = request_json.get('name')
 
-    if value1 is not None:
+    if request.method == 'POST':
+        print("p0")
+        content = request.get_json()
+        valor = content.get('name')
+        fecha = content.get('time')
+        # Guardar valor y fehca en la base de datos sqlite
+        print(valor)
+        print(fecha)
+        return render_template('tabla.html', value1=valor, value2=fecha)
+
+    else:
         print("p3")
-        cursor.execute("INSERT INTO person (first_name,last_name) VALUES (%s,%s)", (value1, value2))
-        data = conn.commit()
-        return jsonify(data)
+        valor=0
+        fecha=0
+        # que los valores de fecha y valor no sean 0 cero sino los que vengan de la base de datos
+        print("NONE", valor, fecha)
+        return render_template('tabla.html', value1=valor, value2=fecha)
 
-    return render_template('hello.html')
+
+app.run(debug=True)
+
+if __name__ == '__name__':
+    app.run(debug=True)

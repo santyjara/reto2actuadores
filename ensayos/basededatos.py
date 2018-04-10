720416b26d2ee225316f9f39b47537a74a087247
Santiago <sjaram25@eafit.edu.co>
2018-04-10 16:55:29 -0500

    reto terminado

--- ensayos/basededatos.py
+++ ensayos/basededatos.py
@@ -3,6 +3,6 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 print ("Opened database successfully");
 
-conn.execute('CREATE TABLE reto(fecha TEXT, valor REAL, clasificacion TEXT')
+conn.execute('CREATE TABLE reto(fecha TEXT, valor REAL, clasificacion TEXT)')
 print ("Table created successfully");
 conn.close()
